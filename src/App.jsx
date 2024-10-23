51130b8c796193de5081b9adfbceccf8976cd8d9
Xuyang <xuyang.wu.dev@gmail.com>
2024-10-23 23:19:31 +0100

    Router initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "menu",
+        element: <Menu />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "order",
+        element: <Order />,
+      },
+      {
+        path: "order/new",
+        element: <CreateOrder />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Pizza</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
